7caee0b36cd8d80edcbc327da5fb98e818be094e
baderlink2 <bouazzaelharyzy@gmail.com>
2025-06-18 17:40:48 +0200

    Add files via upload

---
+++ payload.js
@@ -0,0 +1,39 @@
+function disabled12(){
+LoadedMSG="DISABLED UPDATE Loaded DONE";
+LoadFromGHBLS("disabled_update.bin");
+}
+
+function load_dumper(){
+LoadedMSG="FPKG Dumper V1.2 DONE";
+LoadFromGHBLS("FPKG_DUMP.bin");
+}
+
+function platin(){
+LoadedMSG="Loaded Platinum HEN DONE";
+LoadFromGHBLS("platinum.bin");
+}
+
+function linux0(){
+LoadedMSG="LINX DONE";
+LoadFromGHBLS("linux_barocera.bin");
+}
+
+function restore12(){
+LoadedMSG="restore DB Loaded !!!";
+LoadFromGHBLS("restore.bin");
+}
+
+function backup12(){
+LoadedMSG="Backup DB Loaded !!!";
+LoadFromGHBLS("backup.bin");
+}
+
+function enabled12(){
+LoadedMSG="ENABLED UPDATE DONE";
+LoadFromGHBLS("enabled_update.bin");
+}
+
+function linux1(){
+LoadedMSG="Linux 4G Pro Loaded DONE";
+LoadFromGHBLS("linux4gbpro.bin");
+}
